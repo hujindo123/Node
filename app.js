0482e6ad5cb19eb9f93ab12a2a601dd36e32ca59
Administrator <jd_sof>
2016-08-07 00:18:00 +0800

    push

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var play = require('./routes/play'); //引入play路由
+var aaa = require('./routes/aaa');
 
 var app = express();
 
@@ -22,8 +24,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/play', play); //播放页面
 app.use('/', routes);
 app.use('/users', users);
+app.use('/aaa',aaa);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,15 +50,4 @@ if (app.get('env') === 'development') {
     });
   });
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-app.post("/register.do",routes);
 module.exports = app;
